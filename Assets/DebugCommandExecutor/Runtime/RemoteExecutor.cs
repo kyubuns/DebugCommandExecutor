532754c2ebecf22238ac18a4be9c582e71a3f362
kyubuns <kyubuns@gmail.com>
2024-03-03 17:16:47 +0900

    feat: RemotePlayerはDevelopmentだけ

--- Assets/DebugCommandExecutor/Runtime/RemoteExecutor.cs
+++ Assets/DebugCommandExecutor/Runtime/RemoteExecutor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Networking.PlayerConnection;
 
@@ -9,6 +10,7 @@ namespace DebugCommandExecutor
 #if UNITY_EDITOR || DEVELOPMENT_BUILD
         public static readonly Guid DebugCommandMessage = Guid.Parse("889c3bf6-de3c-4024-aaf3-d6859c32a43f");
 
+        [Conditional("DEVELOPMENT_BUILD")]
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         public static void OnRuntimeInitializeOnLoadMethod()
         {
