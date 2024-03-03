75d25ee7ff47aa30ae87af6418a93607bd37ca4b
kyubuns <kyubuns@gmail.com>
2024-03-03 17:27:20 +0900

    feat: 最低限整える

--- Assets/DebugCommandExecutor/Editor/DebugCommandWindow.cs
+++ Assets/DebugCommandExecutor/Editor/DebugCommandWindow.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEditor;
 using UnityEditor.Networking.PlayerConnection;
 using UnityEngine;
@@ -14,7 +15,8 @@ namespace DebugCommandExecutor.Editor
             window.Show();
         }
 
-        private string _message = "";
+        private int _recipient;
+        private string _message;
 
         private void OnEnable()
         {
@@ -28,28 +30,67 @@ namespace DebugCommandExecutor.Editor
 
         private void OnGUI()
         {
+            _recipient = EditorGUILayout.Popup(_recipient, new[] { "Editor", "Player" });
             _message = EditorGUILayout.TextField("Message:", _message);
 
-            if (GUILayout.Button("Send to Player"))
+            if (_recipient == 0)
             {
-                SendToPlayer(_message);
+                if (Application.isPlaying)
+                {
+                    if (GUILayout.Button("Send to Editor"))
+                    {
+                        SendToEditor(_message);
+                    }
+                }
+                else
+                {
+                    using (_ = new EditorGUI.DisabledGroupScope(true))
+                    {
+                        GUILayout.Button("Send to Editor (Not playing)");
+                    }
+                }
             }
-
-            if (GUILayout.Button("Send to Editor"))
+            else
             {
-                SendToEditor(_message);
+                var connection = EditorConnection.instance;
+                if (connection.ConnectedPlayers.Count > 0)
+                {
+                    if (GUILayout.Button($"Send to Player ({string.Join(", ", connection.ConnectedPlayers.Select(x => x.name))})"))
+                    {
+                        SendToPlayer(_message);
+                    }
+                }
+                else
+                {
+                    using (_ = new EditorGUI.DisabledGroupScope(true))
+                    {
+                        GUILayout.Button("Send to Player (ConnectedPlayers.Count == 0)");
+                    }
+                }
             }
         }
 
         private void SendToPlayer(string message)
         {
             var connection = EditorConnection.instance;
+            if (connection.ConnectedPlayers.Count == 0)
+            {
+                Debug.LogWarning($"DebugCommand | connection.ConnectedPlayers.Count == 0");
+                return;
+            }
+
             var bytes = System.Text.Encoding.UTF8.GetBytes(message);
             connection.Send(RemoteExecutor.DebugCommandMessage, bytes);
         }
 
         private void SendToEditor(string message)
         {
+            if (!Application.isPlaying)
+            {
+                Debug.LogWarning($"DebugCommand | !Application.isPlaying");
+                return;
+            }
+
             DebugCommand.Execute(message);
         }
     }
