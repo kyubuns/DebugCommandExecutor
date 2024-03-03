042959e114ce4c7b829dfd29cdbe5a56dc4a6ef1
kyubuns <kyubuns@gmail.com>
2024-03-03 19:07:50 +0900

    feat: History

--- Assets/DebugCommandExecutor/Editor/DebugCommandWindow.cs
+++ Assets/DebugCommandExecutor/Editor/DebugCommandWindow.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEditor.Networking.PlayerConnection;
@@ -8,29 +9,51 @@ namespace DebugCommandExecutor.Editor
     public class DebugCommandWindow : EditorWindow
     {
         [MenuItem("Tools/Debug Command Executor/Window")]
-        private static void ShowWindow()
+        protected static void ShowWindow()
         {
             var window = GetWindow<DebugCommandWindow>();
             window.titleContent = new GUIContent("Debug Command Window");
             window.Show();
         }
 
+        private const int HistoryMax = 30;
+        private static string EditorPrefsHistoryKey => $"DebugCommand.{Application.productName}";
+
+        private List<string> _history;
         private int _recipient;
         private string _message;
+        private bool _refocusNextFrame;
+        private int _focusHistory = -1;
+        private Vector2 _historyScrollPosition;
 
-        private void OnEnable()
+        protected void OnEnable()
         {
             EditorConnection.instance.Initialize();
         }
 
-        private void OnDisable()
+        protected void OnDisable()
         {
             EditorConnection.instance.DisconnectAll();
         }
 
-        private void OnGUI()
+        protected void Awake()
         {
-            var send = false;
+            _history = EditorPrefs.GetString(EditorPrefsHistoryKey)
+                .Split("\n")
+                .ToList();
+        }
+
+        protected void OnGUI()
+        {
+            var refocus = false;
+
+            {
+                if (_refocusNextFrame)
+                {
+                    _refocusNextFrame = false;
+                    refocus = true;
+                }
+            }
 
             // event
             {
@@ -39,7 +62,31 @@ namespace DebugCommandExecutor.Editor
                 {
                     GUI.FocusControl(null);
                     Send();
-                    send = true;
+                    refocus = true;
+                }
+
+                if (e.type == EventType.KeyDown && e.keyCode == KeyCode.UpArrow)
+                {
+                    if (_focusHistory + 1 < _history.Count)
+                    {
+                        _focusHistory += 1;
+
+                        GUI.FocusControl(null);
+                        _message = _history[_focusHistory];
+                        _refocusNextFrame = true;
+                    }
+                }
+
+                if (e.type == EventType.KeyDown && e.keyCode == KeyCode.DownArrow)
+                {
+                    if (_focusHistory - 1 >= 0)
+                    {
+                        _focusHistory -= 1;
+
+                        GUI.FocusControl(null);
+                        _message = _history[_focusHistory];
+                        _refocusNextFrame = true;
+                    }
                 }
             }
 
@@ -53,22 +100,48 @@ namespace DebugCommandExecutor.Editor
 
                     if (string.IsNullOrEmpty(validate))
                     {
+                        var prevMessage = _message;
                         GUI.SetNextControlName("MessageTextField");
                         _message = EditorGUILayout.TextField(_message);
+
+                        if (_message != prevMessage) _focusHistory = -1;
                     }
                     else
                     {
+                        GUI.FocusControl(null);
                         using (new EditorGUI.DisabledGroupScope(true))
                         {
                             EditorGUILayout.TextField(validate);
                         }
                     }
                 }
+
+                using (var scrollView = new EditorGUILayout.ScrollViewScope(_historyScrollPosition))
+                {
+                    EditorGUILayout.LabelField("History");
+                    for (var i = 0; i < _history.Count; i++)
+                    {
+                        var history = _history[i];
+                        if (GUILayout.Button(history))
+                        {
+                            GUI.FocusControl(null);
+                            _message = history;
+                            _refocusNextFrame = true;
+                            _focusHistory = i;
+                        }
+                    }
+
+                    _historyScrollPosition = scrollView.scrollPosition;
+                }
             }
 
             // event
             {
-                if (send)
+                if (_refocusNextFrame)
+                {
+                    Repaint();
+                }
+                else if (refocus)
                 {
                     GUI.FocusControl("MessageTextField");
                 }
@@ -89,19 +162,25 @@ namespace DebugCommandExecutor.Editor
 
         private void Send()
         {
-            if (string.IsNullOrWhiteSpace(_message)) return;
+            var message = _message.Trim();
+
+            if (string.IsNullOrWhiteSpace(message)) return;
             if (!string.IsNullOrEmpty(Validate())) return;
 
             if (_recipient == 0)
             {
-                SendToEditor(_message);
+                SendToEditor(message);
             }
             else
             {
-                SendToPlayer(_message);
+                SendToPlayer(message);
             }
 
+            SaveHistory(message);
+
             _message = string.Empty;
+            _focusHistory = -1;
+            _historyScrollPosition = Vector2.zero;
         }
 
         private string ValidateSendToEditor()
@@ -127,6 +206,19 @@ namespace DebugCommandExecutor.Editor
             var connection = EditorConnection.instance;
             var bytes = System.Text.Encoding.UTF8.GetBytes(message);
             connection.Send(RemoteExecutor.DebugCommandMessage, bytes);
+        }
+
+        private void SaveHistory(string newMessage)
+        {
+            _history.Remove(newMessage);
+            _history.Insert(0, newMessage);
+
+            while (_history.Count > HistoryMax)
+            {
+                _history.RemoveAt(_history.Count - 1);
+            }
+
+            EditorPrefs.SetString(EditorPrefsHistoryKey, string.Join("\n", _history));
         }
     }
 }
