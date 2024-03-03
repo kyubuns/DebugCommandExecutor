78f0157270821d17a5ec2964e3a66957396759e8
kyubuns <kyubuns@gmail.com>
2024-03-03 18:54:17 +0900

    feat: UI

--- Assets/DebugCommandExecutor/Editor/DebugCommandWindow.cs
+++ Assets/DebugCommandExecutor/Editor/DebugCommandWindow.cs
@@ -17,7 +17,6 @@ namespace DebugCommandExecutor.Editor
 
         private int _recipient;
         private string _message;
-        private bool _refocus;
 
         private void OnEnable()
         {
@@ -31,102 +30,103 @@ namespace DebugCommandExecutor.Editor
 
         private void OnGUI()
         {
-            _recipient = GUILayout.SelectionGrid(_recipient, new[] { "Editor", "Player" }, 2);
+            var send = false;
 
-            GUI.SetNextControlName("MessageTextField");
-            _message = EditorGUILayout.TextField(_message);
-
-            if (_refocus)
+            // event
             {
-                GUI.FocusControl("MessageTextField");
-                _refocus = false;
+                var e = Event.current;
+                if (e.type == EventType.KeyDown && e.keyCode == KeyCode.Return)
+                {
+                    GUI.FocusControl(null);
+                    Send();
+                    send = true;
+                }
             }
 
+            // gui
+            {
+                var validate = Validate();
+
+                using (new EditorGUILayout.HorizontalScope())
+                {
+                    _recipient = GUILayout.SelectionGrid(_recipient, new[] { "Editor", "Player" }, 2, GUILayout.MaxWidth(100));
+
+                    if (string.IsNullOrEmpty(validate))
+                    {
+                        GUI.SetNextControlName("MessageTextField");
+                        _message = EditorGUILayout.TextField(_message);
+                    }
+                    else
+                    {
+                        using (new EditorGUI.DisabledGroupScope(true))
+                        {
+                            EditorGUILayout.TextField(validate);
+                        }
+                    }
+                }
+            }
+
+            // event
+            {
+                if (send)
+                {
+                    GUI.FocusControl("MessageTextField");
+                }
+            }
+        }
+
+        private string Validate()
+        {
             if (_recipient == 0)
             {
-                if (Application.isPlaying)
-                {
-                    if (GUILayout.Button("Send to Editor"))
-                    {
-                        Send();
-                    }
-                }
-                else
-                {
-                    using (_ = new EditorGUI.DisabledGroupScope(true))
-                    {
-                        GUILayout.Button("Send to Editor (not playing)");
-                    }
-                }
+                return ValidateSendToEditor();
             }
             else
             {
-                var connection = EditorConnection.instance;
-                if (connection.ConnectedPlayers.Count > 0)
-                {
-                    if (GUILayout.Button($"Send to Player ({string.Join(", ", connection.ConnectedPlayers.Select(x => x.name))})"))
-                    {
-                        Send();
-                    }
-                }
-                else
-                {
-                    using (_ = new EditorGUI.DisabledGroupScope(true))
-                    {
-                        GUILayout.Button("Send to Player (no players)");
-                    }
-                }
-            }
-
-            var e = Event.current;
-            if ((e.control || e.command) && e.type == EventType.KeyDown && e.keyCode == KeyCode.Return)
-            {
-                Send();
-                e.Use();
+                return ValidateSendToPlayer();
             }
         }
 
         private void Send()
         {
             if (string.IsNullOrWhiteSpace(_message)) return;
+            if (!string.IsNullOrEmpty(Validate())) return;
 
-            bool used;
             if (_recipient == 0)
             {
-                used = SendToEditor(_message);
+                SendToEditor(_message);
             }
             else
             {
-                used = SendToPlayer(_message);
+                SendToPlayer(_message);
             }
 
-            if (used)
-            {
-                _message = string.Empty;
-                _refocus = true;
-                GUI.FocusControl(null); // 一回フォーカスを外さないとTextAreaが正しくリセットされない
-                Repaint();
-            }
+            _message = string.Empty;
         }
 
-        private static bool SendToPlayer(string message)
+        private string ValidateSendToEditor()
+        {
+            if (!Application.isPlaying) return "not playing";
+            return null;
+        }
+
+        private static void SendToEditor(string message)
+        {
+            DebugCommand.Execute(message);
+        }
+
+        private string ValidateSendToPlayer()
         {
             var connection = EditorConnection.instance;
-            if (connection.ConnectedPlayers.Count == 0) return false;
-
-            var bytes = System.Text.Encoding.UTF8.GetBytes(message);
-            connection.Send(RemoteExecutor.DebugCommandMessage, bytes);
-
-            return true;
+            if (connection.ConnectedPlayers.Count == 0) return "no player";
+            return null;
         }
 
-        private static bool SendToEditor(string message)
+        private static void SendToPlayer(string message)
         {
-            if (!Application.isPlaying) return false;
-
-            DebugCommand.Execute(message);
-
-            return true;
+            var connection = EditorConnection.instance;
+            var bytes = System.Text.Encoding.UTF8.GetBytes(message);
+            connection.Send(RemoteExecutor.DebugCommandMessage, bytes);
         }
     }
 }
